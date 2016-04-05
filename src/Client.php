493f341d5ac80d5dcd136a66810b19adda511085
Nicky Woolf <woolfnicky@gmail.com>
2016-04-05 07:12:56 -0700

    Client can decode and parse response object.

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,9 @@
 
 namespace Woolf\Shophpify;
 
+use GuzzleHttp\Client as GuzzleClient;
+use Psr\Http\Message\ResponseInterface;
+
 class Client
 {
     protected $clientId;
@@ -19,6 +22,31 @@ class Client
         $this->accessToken = $accessToken;
     }
 
+    public function get($url, array $options = [])
+    {
+        return $this->http()->get($url, $options);
+    }
+
+    public function post($url, array $options = [])
+    {
+        return $this->http()->post($url, $this->prepare($options));
+    }
+
+    public function put($url, array $options = [])
+    {
+        return $this->http()->put($url, $this->prepare($options));
+    }
+
+    public function delete($url, array $options = [])
+    {
+        return $this->http()->delete($url, $this->prepare($options));
+    }
+
+    protected function http()
+    {
+        return new GuzzleClient($this->config());
+    }
+
     public function config()
     {
         return (! is_null($this->accessToken)) ? $this->tokenHeader() : [];
@@ -27,5 +55,21 @@ class Client
     public function tokenHeader()
     {
         return ['headers' => ['X-Shopify-Access-Token' => $this->accessToken]];
+    }
+
+    protected function prepare(array $options = [])
+    {
+        return (empty($options)) ? $options : ['form_params' => $options];
+    }
+
+    public function parse(ResponseInterface $response, $extract = false)
+    {
+        $response = json_decode($response->getBody(), true);
+
+        if ($extract) {
+            return isset($response[$extract]) ? $response[$extract] : false;
+        }
+
+        return $response;
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -10,9 +10,27 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $client = new Client('client_id', 'client_secret', 'access_token');
 
         $this->assertArrayHasKey('headers', $client->config());
-
         $this->assertArrayHasKey('X-Shopify-Access-Token', $client->config()['headers']);
-
         $this->assertEquals('access_token', $client->config()['headers']['X-Shopify-Access-Token']);
+    }
+
+    /** @test */
+    function it_builds_client_with_empty_array_as_config_if_no_access_token_provided()
+    {
+        $client = new Client('client_id', 'client_secret');
+
+        $this->assertEmpty($client->config());
+    }
+
+    /** @test */
+    function it_can_decode_and_parse_response_object()
+    {
+        $response = new \GuzzleHttp\Psr7\Response(200, [], json_encode(['works' => true]));
+
+        $client = new Client('client_id', 'client_secret');
+
+        $this->assertEquals(['works' => true], $client->parse($response));
+        $this->assertTrue($client->parse($response, 'works'));
+        $this->assertFalse($client->parse($response, 'key_does_not_exist'));
     }
 }
