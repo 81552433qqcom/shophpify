5a61a6031ef1be771171c2e9b915d10a0af47afe
Nicky Woolf <woolfnicky@gmail.com>
2016-04-10 08:47:59 -0700

    Create base test case for resources.

--- tests/Resource/AbandonedCheckoutTest.php
+++ tests/Resource/AbandonedCheckoutTest.php
@@ -1,27 +1,10 @@
 <?php
 
 use Woolf\Shophpify\Resource\AbandonedCheckout;
+use Woolf\Shophpify\Testing\Resource\ResourceTestCase;
 
-class AbandonedCheckoutTest extends PHPUnit_Framework_TestCase
+class AbandonedCheckoutTest extends ResourceTestCase
 {
-    public static function setUpBeforeClass()
-    {
-        if (! file_exists(__DIR__.'/../../.env')) {
-            throw new Exception('No .env file found');
-        }
-
-        (new \Dotenv\Dotenv(__DIR__.'/../../'))->load();
-    }
-
-    protected function client()
-    {
-        return new \Woolf\Shophpify\Client(getenv('SHOPIFY_ACCESS_TOKEN'));
-    }
-
-    protected function endpoint()
-    {
-        return new \Woolf\Shophpify\Endpoint(getenv('SHOPIFY_STORE_DOMAIN'));
-    }
 
     /** @test */
     function it_gets_of_abandoned_checkouts_for_shop()
---
+++ tests/Resource/ResourceTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Woolf\Shophpify\Testing\Resource;
+
+use Dotenv\Dotenv;
+use Exception;
+use PHPUnit_Framework_TestCase;
+use Woolf\Shophpify\Client;
+use Woolf\Shophpify\Endpoint;
+
+abstract class ResourceTestCase extends PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        if (! file_exists(__DIR__.'/../../.env')) {
+            throw new Exception('No .env file found');
+        }
+
+        (new Dotenv(__DIR__.'/../../'))->load();
+    }
+
+    protected function client()
+    {
+        return new Client(getenv('SHOPIFY_ACCESS_TOKEN'));
+    }
+
+    protected function endpoint()
+    {
+        return new Endpoint(getenv('SHOPIFY_STORE_DOMAIN'));
+    }
+}
